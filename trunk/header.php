6adc5bcfddcb4bf9bf2a598735d0be947e39fa92
swallick <swallick@9d407e5a-fa1a-0410-9d15-71b1442d8d04>
2007-01-20 04:15:05 +0000

    Fixed hover links for sub-pages/-categories; cleaned up header.

--- trunk/header.php
+++ trunk/header.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php if ( !function_exists('language_attributes') || language_attributes() ) : ?>xml:lang="en-us" lang="en-us"<?php endif; ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php if ( !function_exists('language_attributes') || language_attributes() ) : ?>xml:lang="EN-us" lang="EN-us"<?php endif; ?>>
 <head profile="http://gmpg.org/xfn/11">
 	<title><?php bloginfo('name') ?><?php if ( is_404() ) : ?> / <?php _e('Page not found', 'plaintxtblog') ?><?php elseif ( is_home() ) : ?> / <?php bloginfo('description') ?><?php elseif ( is_category() ) : ?> / <?php echo single_cat_title(); ?><?php elseif ( is_date() ) : ?> / Blog Archives<?php elseif ( is_search() ) : ?> / Search Results<?php else : ?> / <?php the_title() ?><?php endif ?></title>
 	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 	<meta name="generator" content="WordPress <?php bloginfo('version') ?>" /><!-- LEAVE FOR STATS -->
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
-	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php bloginfo('name') ?> RSS 2.0 Feed" />
-	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php bloginfo('name') ?> Comments RSS 2.0 Feed" />
+	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php bloginfo('name') ?> RSS feed" />
+	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php bloginfo('name') ?> comments RSS feed" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
-	<link rel="start" href="<?php echo get_settings('home') ?>/" title="<?php bloginfo('name') ?>" />
 	<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('stylesheet_url'); ?>" title="plaintxtBlog" />
 	<link rel="stylesheet" type="text/css" media="print" href="<?php bloginfo('template_directory'); ?>/print.css" />
 
