854c559f699eff891c24e91bea609dcfa4f4481b
swallick <swallick@9d407e5a-fa1a-0410-9d15-71b1442d8d04>
2007-11-08 17:20:30 +0000

    -Update to WP 2.3 compatibility -Removed all WP 2.0 stuff -Now with tags! -Added tagged versions (previous releases)

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form id="searchform" method="get" action="<?php bloginfo('home'); ?>/">
+	<div>
+		<input type="text" name="s" id="s" size="5" tabindex="1" />
+		<input type="submit" name="searchsubmit" id="searchsubmit" value="Find" tabindex="2"/>
+	</div>
+</form>
